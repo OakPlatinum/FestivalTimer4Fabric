813768cf0152c57f0ad5c239675e100aeda19f2a
Oak Platinum <oakplatinum@qq.com>
2025-01-26 21:29:05 +0800

    Fix Config.java

--- src/main/java/com/hz6826/festivaltimer/Config.java
+++ src/main/java/com/hz6826/festivaltimer/Config.java
@@ -7,37 +7,37 @@ import java.util.List;
 public class Config extends MidnightConfig {  // festival_timer.json
     public static final String BASIC = "Basic";
 
-    @Entry(category = BASIC, name = "enable")
+    @Entry(name = "enable")
     public static boolean enable = true;
-    @Entry(category = BASIC, name = "target-date-time")
+    @Entry(name = "target-date-time")
     public static String targetDateTime = "2025-01-29 00:00:00";
-    @Entry(category = BASIC, name = "boss-bar-color")  // By name
+    @Entry(name = "boss-bar-color")  // By name
     public static String bossBarColorName = "pink";
-    @Entry(category = BASIC, name = "last-ten-seconds-color-code")  // By name
+    @Entry(name = "last-ten-seconds-color-code")  // By name
     public static String lastTenSecondsColorName = "gold";
 
-    @Entry(category = BASIC, name = "title-remidner-time")
+    @Entry(name = "title-remidner-time")
     public static int titleReminderTime = 30;
-    @Entry(category = BASIC, name = "title-stay-time")
+    @Entry(name = "title-stay-time")
     public static int titleStayTime = 20;
 
-    @Entry(category = BASIC, name = "prefix")
+    @Entry(name = "prefix")
     public static String prefix = "§4[FestivalTimer]§r ";
 
-    @Entry(category = BASIC, name = "boss-bar-message")
+    @Entry(name = "boss-bar-message")
     public static String bossBarMessage = "距活动开始还有：%d天 %d时 %d分 %d秒";
-    @Entry(category = BASIC, name = "end-boss-bar-message")
+    @Entry(name = "end-boss-bar-message")
     public static String endBossBarMessage = "倒计时已结束";
 
-    @Entry(category = BASIC, name = "reload-config-message")
+    @Entry(name = "reload-config-message")
     public static String reloadConfigMessage = "配置已重新加载！";
 
-    @Entry(category = BASIC, name = "title")
+    @Entry(name = "title")
     public static String endTitleMessage = "新年快乐！";
-    @Entry(category = BASIC, name = "subtitle.enable")
+    @Entry(name = "subtitle.enable")
     public static boolean enableEndSubTitleMessage = false;
-    @Entry(category = BASIC, name = "subtitle.text")
+    @Entry(name = "subtitle.text")
     public static String endSubTitleMessage = "这是倒计时结束后的副标题";
-    @Entry(category = BASIC, name = "sound.enable")
+    @Entry(name = "sound.enable")
     public static boolean enableEndSound = true;
 }
